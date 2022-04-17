2c97c70743af46cfa36936c6e7ded5f6bc90bd95
monirulislam9549 <monirulislam9549@gmail.com>
2022-04-17 15:26:42 +0600

    banner added

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -12,7 +12,7 @@ const Services = () => {
     }, [])
     return (
         <div className='container'>
-            <h1 className='title'>Services</h1>
+            <h1 className='title mt-5'>Services</h1>
             <div className='services-container'>
                 {
                     services.map(service => <Service
