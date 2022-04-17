2953af60791f1880551805c41a05cd6c3d9804a5
monirulislam9549 <monirulislam9549@gmail.com>
2022-04-17 16:55:45 +0600

    navbar modified

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import logo from '../../../images/logo teeth.png'
 
 const Header = () => {
     return (
         <>
-            <Navbar bg="dark" variant="dark">
+            <Navbar bg="dark" sticky='top' variant="dark " style={{ height: '75px' }}>
                 <Container>
-                    <Navbar.Brand href="#home"><img src={logo} alt="" /></Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/"><img src={logo} alt="" /></Navbar.Brand>
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+
+                        <Nav.Link as={Link} to="about">About</Nav.Link>
+                        <Nav.Link as={Link} to="login">Login</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
